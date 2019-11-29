0118b2c91b7377af8223ed1c3ac70cb96bb7d6e8
sharonbenaharon <58299693+sharonbenaharon@users.noreply.github.com>
2019-11-29 18:45:26 +0200

    Update Mybank.h

--- Mybank.h
+++ Mybank.h
@@ -4,7 +4,7 @@
 #define ROWS 50
 #include<stdlib.h>
 #include <time.h>
-
+extern float arr[ROWS][COLS];
 int Opening_an_account(float initial_amount);
 void Balance_inquiry(int Account_Number);
 void Deposit(int Account_Number);
